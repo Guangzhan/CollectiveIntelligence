e66bc0734108e9b71c98154f0fefdbe5bd557a59
Guangzhan <fuzhan317@gmail.com>
2016-07-30 16:08:47 +0800

    commit  change

---
+++ MakingRecommendations/recommendations.py
@@ -0,0 +1,10 @@
+# *-* coding:utf-8 *-*
+# author:Guangzhan
+
+critics = {'Lise Rose': {'Lady in the water': 2.5, 'Just my Luck': 3.0,
+                         'Snakes on a Plane': 3.5, 'Superman Back':3.5,
+                         'You, Me and Dupree':2.5, 'The Night Listener': 3.0},
+           'Gene Syemour': {'Lady in the water': 3.0, 'Snakes on a plane':3.5,
+                            'Just my Luck': 1.5, 'Superman Back': 5.0,
+                            'The Night Listener': 3.0, 'You, Me and Dupree': 3.5}
+           }
