ed860234a9b96b7b2c9ddfd4f7a92a2617554f08
Guangzhan <fuzhan317@gmail.com>
2016-07-30 16:16:33 +0800

    commit  change

--- MakingRecommendations/recommendations.py
+++ MakingRecommendations/recommendations.py
@@ -2,9 +2,11 @@
 # author:Guangzhan
 
 critics = {'Lise Rose': {'Lady in the water': 2.5, 'Just my Luck': 3.0,
-                         'Snakes on a Plane': 3.5, 'Superman Back':3.5,
-                         'You, Me and Dupree':2.5, 'The Night Listener': 3.0},
-           'Gene Syemour': {'Lady in the water': 3.0, 'Snakes on a plane':3.5,
+                         'Snakes on a Plane': 3.5, 'Superman Back': 3.5,
+                         'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},
+           'Gene Syemour': {'Lady in the water': 3.0, 'Snakes on a Plane': 3.5,
                             'Just my Luck': 1.5, 'Superman Back': 5.0,
-                            'The Night Listener': 3.0, 'You, Me and Dupree': 3.5}
+                            'The Night Listener': 3.0, 'You, Me and Dupree': 3.5},
+           'Michael Phillips': {'Lady in the water': 3.0, 'Snakes on a Plane': 3.0,
+                                'Superman Back': 3.5, 'The Night Listener': 4.0}
            }
